293a79396a0d39f651e469ba5b468d2d73ed2d92
Kate <kateryna672009@gmail.com>
2024-04-10 14:32:14 +0300

    change burget button

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -1,9 +1,9 @@
 const hamburger = document.querySelector('.hamburger')
 const headerNav = document.querySelector('.header-nav')
 
-hamburger.addEventListener('click', function(){
-    hamburger.classList.toggle('is-active')
-    headerNav.classList.toggle('show')
+hamburger.addEventListener('click', function() {
+  hamburger.classList.toggle('is-active');
+  headerNav.classList.toggle('show');
 })
 
 
